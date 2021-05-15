b3add058d7d012af542a669b11599246ed23bda3
Vigdis-Irene Steinsund <vigdisis@stud.ntnu.no>
2021-05-15 02:33:40 +0200

    adds rest of tweets

--- generate_tweetsets.py
+++ generate_tweetsets.py
@@ -4,20 +4,27 @@ from tweepy import OAuthHandler, API, Cursor
 
 import time
 
+import os
+
+CONS_KEY = os.getenv('CONS_KEY')
+CONS_SECRET = os.getenv('CONS_SECRET')
+ACC_TOKEN = os.getenv('ACC_TOKEN')
+ACC_TOKEN_SECRET = os.getenv('ACC_TOKEN_SECRET')
+
 # Personal tokens
 from unidecode import unidecode
 
-consumer_key = 'nKocnaxNQwsFrQHLHjJ08tu7y'
-consumer_secret = '98bqiL8UebuTHMZshLjv2QqrWafU3Hb2JOZ7wbZN4ETSRbzmZg'
-access_token = '1089555060722491393-rOsJn0i0HL9DtC3CoC8c0Ef4Knogtr'
-access_token_secret = 'XHTP4QFdpo7p9fP3EmoJaMn24F677ACbgJ7U2p6WZJPgQ'
+consumer_key = CONS_KEY
+consumer_secret = CONS_SECRET
+access_token = ACC_TOKEN
+access_token_secret = ACC_TOKEN_SECRET
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = API(auth)
 
 # Usernames whose tweets we want to gather.
-users = ["rileyreidx3"]
+users = []
 
 for user in users:
 
